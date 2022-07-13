8795ac38f0b6686abbac8bee39a9894084cfde26
Julien <julien@ripouteau.com>
2022-07-13 23:34:23 +0200

    feat: allow to pass a default scale and class for all icons

--- tests/generate_svg.spec.ts
+++ tests/generate_svg.spec.ts
@@ -10,71 +10,109 @@
 import { test } from '@japa/runner'
 import { icons } from '@iconify-json/mdi'
 import { addIcon } from '@iconify/iconify'
-import { generateSvg } from '../src/generate_svg'
+import { SvgGenerator } from '../src/svg_generator'
 
 addIcon('mdi:home', { ...icons.icons.home, width: 24, height: 24 })
 
-test.group('generateSvg', () => {
+const svgGenerator = new SvgGenerator()
+
+test.group('svgGenerator.generate', () => {
   test('generate svg for a given icon', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home'),
+      svgGenerator.generate('mdi:home'),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">${icons.icons.home.body}</svg>`
     )
   })
 
   test('define hFlip customization', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { hFlip: true }),
+      svgGenerator.generate('mdi:home', { hFlip: true }),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)">${icons.icons.home.body}</g></svg>`
     )
   })
 
   test('define vFlip customization', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { vFlip: true }),
+      svgGenerator.generate('mdi:home', { vFlip: true }),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g transform="translate(0 24) scale(1 -1)">${icons.icons.home.body}</g></svg>`
     )
   })
 
   test('define both vFlip and hFlip customizations', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { vFlip: true, hFlip: true }),
+      svgGenerator.generate('mdi:home', { vFlip: true, hFlip: true }),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g transform="rotate(180 12 12)">${icons.icons.home.body}</g></svg>`
     )
   })
 
   test('define rotate customization', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { rotate: 1 }),
+      svgGenerator.generate('mdi:home', { rotate: 1 }),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g transform="rotate(90 12 12)">${icons.icons.home.body}</g></svg>`
     )
   })
 
   test('define custom width and height customizations', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { width: 40, height: 40 }),
+      svgGenerator.generate('mdi:home', { width: 40, height: 40 }),
       `<svg width="40" height="40" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">${icons.icons.home.body}</svg>`
     )
   })
 
   test('define color customizations', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { color: 'red' }),
+      svgGenerator.generate('mdi:home', { color: 'red' }),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" color="red">${icons.icons.home.body}</svg>`
     )
   })
 
   test('define custom props', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { class: 'w-4 h-4' }),
+      svgGenerator.generate('mdi:home', { class: 'w-4 h-4' }),
       `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" class="w-4 h-4">${icons.icons.home.body}</svg>`
     )
   })
 
   test('do not set width and height when size is set to none', ({ assert }) => {
     assert.equal(
-      generateSvg('mdi:home', { size: 'none', class: 'w-4 h-4' }),
+      svgGenerator.generate('mdi:home', { size: 'none', class: 'w-4 h-4' }),
       `<svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" class="w-4 h-4">${icons.icons.home.body}</svg>`
+    )
+  })
+
+  test('define base scale', ({ assert }) => {
+    const generator = new SvgGenerator({ scale: 4 })
+
+    assert.equal(
+      generator.generate('mdi:home'),
+      `<svg width="4em" height="4em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">${icons.icons.home.body}</svg>`
+    )
+  })
+
+  test('overwrite base scale', ({ assert }) => {
+    const generator = new SvgGenerator({ scale: 4 })
+
+    assert.equal(
+      generator.generate('mdi:home', { width: 40, height: 40 }),
+      `<svg width="40" height="40" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">${icons.icons.home.body}</svg>`
+    )
+  })
+
+  test('define base classes', ({ assert }) => {
+    const generator = new SvgGenerator({ defaultClass: 'text-red hover:text-blue' })
+
+    assert.equal(
+      generator.generate('mdi:home'),
+      `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" class="text-red hover:text-blue">${icons.icons.home.body}</svg>`
+    )
+  })
+
+  test('combine base and custom classes', ({ assert }) => {
+    const generator = new SvgGenerator({ defaultClass: 'text-red hover:text-blue' })
+
+    assert.equal(
+      generator.generate('mdi:home', { class: 'w-4 h-4 text-yellow' }),
+      `<svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" class="w-4 h-4 text-yellow text-red hover:text-blue">${icons.icons.home.body}</svg>`
     )
   })
 })
